7138d8f235c1eddbe24d685a46f33c009545fc07
25-cs-042 <25-cs-042@student.hitecuni.edu.pk>
2026-06-21 12:34:25 -0700

    Update Invoice.cpp
    Signed-off-by: 25-cs-042 <25-cs-042@student.hitecuni.edu.pk>

--- finance/Invoice.cpp
+++ finance/Invoice.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    Invoice.cpp
  * @brief   Implementation of the Invoice class
- * @author  Ahmad Ali (25-CS-067), Umer Altaf (25-CS-057), Muhammed Ahmad (25-CS-252)
+ * @author  Ahmad Habib(--042) Ali Hammad (--029)
  * @course  CS-104L: Object-Oriented Programming
  * @inst    HITEC University Taxila
  * @date    2026-06-05
